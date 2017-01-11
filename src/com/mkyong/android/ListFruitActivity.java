6f490931d66dea3a58489fa1d3cd29be490357e5
kondi-himabindu <himab8790@gmail.com>
2017-01-12 00:40:31 +0530

    fdfdf

--- src/com/mkyong/android/ListFruitActivity.java
+++ src/com/mkyong/android/ListFruitActivity.java
@@ -14,7 +14,7 @@ public class ListFruitActivity extends ListActivity {
 
 	static final String[] FRUITS = new String[] { "Apple", "Avocado", "Banana",
 			"Blueberry", "Coconut", "Durian", "Guava", "Kiwifruit",
-			"Jackfruit", "Mango", "Olive", "Pear", "Sugar-apple" };
+			"Jackfruit", "Mango", "Olive", "Pear", "Sugar-apple" , };
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
